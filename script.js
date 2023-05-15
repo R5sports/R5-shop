1f2cc051fccb302075e2f0ffc477b041779fcc8e
R5sports <133589781+R5sports@users.noreply.github.com>
2023-05-15 18:23:19 +0300

    Website beta

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+}
